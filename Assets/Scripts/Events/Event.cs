9ee5fe354beca996752ab206bb5176db628f5091
Crenantious <66210196+Crenantious@users.noreply.github.com>
2022-08-06 00:46:32 +0100

    Update Event and EventInfo
    EventInfo is now a SciptableObject So values can be assigned in the inspector.
    
    Event asset menu is now under "Event/Event" to group with EventInfo.

--- Assets/Scripts/Events/Event.cs
+++ Assets/Scripts/Events/Event.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[CreateAssetMenu(fileName = "Event", menuName = "Event")]
+[CreateAssetMenu(fileName = "Event", menuName = "Event/Event")]
 public class Event : ScriptableObject
 {
     UnityEvent e;
